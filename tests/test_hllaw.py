49f99be40806adc8ce649608be6c251397adc04b
fhopecc <fhopecc@gmail.com>
2022-12-12 13:57:59 +0800

    Update test_hllaw.py

--- tests/test_hllaw.py
+++ tests/test_hllaw.py
@@ -7,9 +7,13 @@ class Test(unittest.TestCase):
 
     def test(self):
         from zhongwen.花蓮縣法規 import 法規
-        df = 法規().query('法規體系=="消防"')
-        # df = df[['法規名稱', '公發布日']]
-        # df = df.query('')
+        # df = cache.get('花蓮縣法規')
+        # print(df)
+        # for k in cache.iterkeys():
+            # print(k)
+        # print(cache.iterkeys())
+        df = 法規().query('法規體系=="建設"')
+        df = df[['法規名稱', '公發布日']]
         df.reset_index(inplace=True)
         df = df.iloc[:100]
         html = Path.home() / 'TEMP' / 'output.html'
