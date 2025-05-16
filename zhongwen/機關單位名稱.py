32425945ca8619529944cf0eb0151341936e2806
fhopecc <fhopecc@gmail.com>
2025-05-16 08:38:55 +0800

    建立機關單位名稱分析

---
+++ zhongwen/機關單位名稱.py
@@ -0,0 +1,35 @@
+市縣 = ["臺北市" ,"新北市" ,"桃園市" ,"臺中市" ,"臺南市" ,"高雄市"
+,"基隆市" ,"宜蘭縣" ,"新竹縣" ,"新竹市" ,"苗栗縣" ,"彰化縣" ,"南投縣"
+,"雲林縣" ,"嘉義縣" ,"嘉義市" ,"屏東縣" ,"花蓮縣" ,"臺東縣" ,"澎湖縣"
+,"金門縣" ,"連江縣"]
+
+def 取歸屬市縣(名稱, 排除市縣='宜蘭縣'):
+    應彙整市縣 = set(市縣) - set(排除市縣)
+    for n in list(應彙整市縣):
+        if n in 名稱:
+            return n
+    if '桃' in 名稱:
+        if not '桃園市' in 排除市縣: return '桃園市'
+    if '高雄' in 名稱:
+        if not '高雄市' in 排除市縣: return '高雄市'
+    if '基' in 名稱:
+        if not '基隆市' in 排除市縣: return '基隆市'
+    if '竹縣' in 名稱:
+        if not '新竹縣' in 排除市縣: return '新竹縣'
+    if '苗' in 名稱:
+        if not '苗栗縣' in 排除市縣: return '苗栗縣'
+    if '雲林' in 名稱:
+        if not '雲林縣' in 排除市縣: return '雲林縣'
+
+def 未回覆市縣(回覆文件目錄, 排除市縣='宜蘭縣'):
+    已回覆市縣 = []
+    for fn in 回覆文件目錄.glob("*"):
+        回覆市縣 = 取歸屬市縣(fn.stem, 排除市縣=排除市縣)
+        if 回覆市縣:
+            已回覆市縣.append(回覆市縣)
+    未回覆市縣 = set(市縣) - set(已回覆市縣) - set(排除市縣)
+    return 未回覆市縣
+
+if __name__ == '__main__':
+    from pathlib import Path
+    print(未回覆市縣(Path(__file__).parent))
