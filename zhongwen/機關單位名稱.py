6951e32bffe30bfea82673806e3f510d5635cbdc
fhopecc <fhopecc@gmail.com>
2025-05-16 10:22:58 +0800

    增加排除市縣功能

--- zhongwen/機關單位名稱.py
+++ zhongwen/機關單位名稱.py
@@ -16,18 +16,32 @@ def 取歸屬市縣(名稱, 排除市縣='宜蘭縣'):
         if not '基隆市' in 排除市縣: return '基隆市'
     if '竹縣' in 名稱:
         if not '新竹縣' in 排除市縣: return '新竹縣'
+    if '竹市' in 名稱:
+        if not '新竹市' in 排除市縣: return '新竹市'
     if '苗' in 名稱:
         if not '苗栗縣' in 排除市縣: return '苗栗縣'
+    if '彰' in 名稱:
+        if not '彰化縣' in 排除市縣: return '彰化縣'
     if '雲林' in 名稱:
         if not '雲林縣' in 排除市縣: return '雲林縣'
+    if '嘉市' in 名稱:
+        if not '嘉義市' in 排除市縣: return '嘉義市'
+    if '花' in 名稱:
+        if not '花蓮縣' in 排除市縣: return '花蓮縣'
+    if '澎' in 名稱:
+        if not '澎湖縣' in 排除市縣: return '澎湖縣'
 
 def 未回覆市縣(回覆文件目錄, 排除市縣='宜蘭縣'):
+    from collections.abc import Iterable 
+    if isinstance(排除市縣, str) or not isinstance(排除市縣, Iterable):
+        排除市縣 = [排除市縣]
     已回覆市縣 = []
     for fn in 回覆文件目錄.glob("*"):
         回覆市縣 = 取歸屬市縣(fn.stem, 排除市縣=排除市縣)
         if 回覆市縣:
             已回覆市縣.append(回覆市縣)
-    未回覆市縣 = set(市縣) - set(已回覆市縣) - set(排除市縣)
+    未回覆市縣 = set(市縣) - set(已回覆市縣)
+    未回覆市縣 -= set(排除市縣)
     return 未回覆市縣
 
 if __name__ == '__main__':
