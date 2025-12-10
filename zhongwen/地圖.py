f9ca23b5d93232ce088e8e15e02ef5fb326dd7f3
fhopecc <fhopecc@gmail.com>
2025-12-10 16:20:29 +0800

    增加地圖功能

--- zhongwen/地圖.py
+++ zhongwen/地圖.py
@@ -1,8 +1,10 @@
-def show_map(gdf):
+def 顯示地圖(gdf):
     '運用 folium 顯示 geopandas 資料框'
     from shapely.geometry import mapping
     from pathlib import Path
+    import tempfile
     import folium
+    import time
     import os
     gdf = gdf.to_crs(epsg='4326')
     m = folium.Map(location=[gdf.centroid.y.mean(), gdf.centroid.x.mean()], zoom_start=10)
@@ -11,6 +13,8 @@ def show_map(gdf):
         geojson = mapping(row.geometry)
         folium.GeoJson(geojson).add_to(m)
 
-    html = Path.home() / 'TEMP' / 'output.html'
-    m.save(html)
-    os.system(f'start {html}')
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        html = os.path.join(tmpdirname, "tempfile.html")
+        m.save(html)
+        os.system(f'start {html}')
+        time.sleep(10)
